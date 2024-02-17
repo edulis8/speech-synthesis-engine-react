655e384c2e84b936461aea0c0b9fbabafbcb38ff
Eric Broberg <eric.edulis@gmail.com>
2024-02-16 16:53:38 -0800

    provisional mock on getVoices to fix an erroneous test failure in test suite - Speechify team, please review

--- src/tests/useSpeech.test.ts
+++ src/tests/useSpeech.test.ts
@@ -1,18 +1,19 @@
-import {
-  describe,
-  expect,
-  it,
-} from 'vitest';
+import { describe, expect, it } from "vitest";
 
-import {
-  act,
-  renderHook,
-} from '@testing-library/react';
+import { act, renderHook } from "@testing-library/react";
 
-import { useSpeech } from '../lib/useSpeech';
+import { useSpeech } from "../lib/useSpeech";
 
 describe("useSpeech Test Suite", () => {
   it("should return current sentence idx and current word range as well as playback state", () => {
+    
+    // this test was failing due to
+    // "TypeError: Cannot read properties of undefined (reading 'getVoices')"
+    // provisional solution: Mock getVoices 
+    window.speechSynthesis = {
+      getVoices: () => [1],
+    };
+
     const sentences = ["This is a sentence.", "This is another sentence."];
     const { result } = renderHook(() => useSpeech(sentences));
     expect(result.current.currentSentenceIdx).toBe(0);
